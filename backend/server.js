1e6b36826b0c5f26950cb68a6929890d0c82fec7
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-07-23 16:25:07 +0330

    basic struct added

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const https = require('https')
+
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+const key = fs.readFileSync('./config/cert.key');
+const cert = fs.readFileSync('./config/cert.crt');
+const options = { key, cert }
+const httpsServer = https.createServer(options, app);
+const socketio = require('socket.io');
+const mediasoup = require('mediasoup');
+
+const io = socketio(httpsServer, {
+    cors: ['https://localhost:3030']
+})
+
+
+httpsServer.listen(3030);
